0e02227f877f365d11f9f59d7dfd7bfb9948f513
Siddhartha Raju <raju.s@husky.neu.edu>
2020-03-27 05:34:35 -0400

    Initial Commit Angular Project

---
+++ assignment8/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoCreateComponent,
+    TodoEditComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
