ac0e692ccf296ee85e5778eb915e0ef26102b16a
Siddhartha Raju <raju.s@husky.neu.edu>
2020-03-27 05:42:34 -0400

    Updating routes

--- assignment8/src/app/app.module.ts
+++ assignment8/src/app/app.module.ts
@@ -1,11 +1,12 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoCreateComponent } from './todo-create/todo-create.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
