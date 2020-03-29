46d9d26d37d27a9e244317038ba0af0bd980e938
Siddhartha Raju <raju.s@husky.neu.edu>
2020-03-29 18:46:25 -0400

    Adding server

--- assignment8/src/app/app.module.ts
+++ assignment8/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductGetComponent } from './product-get/product-get.component';
-import { TodoGetComponent } from './todo-get/todo-get.component';
-import { TodoCreateComponent } from './todo-create/todo-create.component';
-import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductAddComponent,
-    ProductGetComponent,
-    TodoGetComponent,
-    TodoCreateComponent,
-    TodoEditComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
